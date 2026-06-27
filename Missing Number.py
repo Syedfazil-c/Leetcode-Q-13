914a99433e64a22f588cb1bd1839196b32bfb1b5
Syedfazil-c <syedfazil3956@gmail.com>
2026-06-27 22:19:11 +0530

    Missing Number

---
+++ Missing Number.py
@@ -0,0 +1,9 @@
+def missingNumber(nums):
+    n = len(nums)
+    expected_sum = n * (n + 1) // 2
+    actual_sum = sum(nums)
+    return expected_sum - actual_sum
+
+
+nums = [1, 3, 0]
+print(missingNumber(nums))
